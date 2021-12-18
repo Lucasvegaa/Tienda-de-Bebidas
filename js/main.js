7003c9b03afaed05c415882a8cceb7316acc937d
Lucasvegaa <lucasvegaa@gmail.com>
2021-12-18 13:07:57 -0300

    add all method tienda

--- js/main.js
+++ js/main.js
@@ -15,38 +15,43 @@ class Carrito {
         this.productos = productos;
     }
 
-    agregarAlCarrito(carrito) {
-        let valorID = 0;
-        let total = 0;
-        let articulos = 0;
-        carrito.productos = [];
+    // agregarAlCarrito(carrito) {
+    //     let valorID = 0;
+    //     let total = 0;
+    //     let articulos = 0;
+    //     carrito.productos = [];
 
-        do {
+    //     do {
 
-            valorID = parseInt(prompt("Ingrese el ID del producto que desea agregar: \n(lista de productos en consola) \n **Para salir ingrese 0 **\nCantidad Productos: " + articulos + " \nTOTAL: " + total));
-            if (valorID != 0) {
-                let producto = tienda.buscarProductoPorId(valorID);
-                if (producto != undefined) {
-                    this.productos.push(producto);
-                    total = carrito.TotalCarrito(this.productos);
-                    articulos++;
-                }
-                else {
-                    alert("ID no es valido")
-                }
-            }
+    //         valorID = parseInt(prompt("Ingrese el ID del producto que desea agregar: \n(lista de productos en consola) \n **Para salir ingrese 0 **\nCantidad Productos: " + articulos + " \nTOTAL: " + total));
+    //         if (valorID != 0) {
+    //             let producto = tienda.buscarProductoPorId(valorID);
+    //             if (producto != undefined) {
+    //                 this.productos.push(producto);
+    //                 total = carrito.TotalCarrito(this.productos);
+    //                 articulos++;
+    //             }
+    //             else {
+    //                 alert("ID no es valido")
+    //             }
+    //         }
 
 
-        } while (valorID != 0);
-        console.clear();
-        console.log("Su pedido es: ");
-        carrito.listarProductos(this.productos);
-        console.log("Usted cargo: " + articulos + " productos y el total va a pagar es de : $" + total);
+    //     } while (valorID != 0);
+    //     console.clear();
+    //     console.log("Su pedido es: ");
+    //     carrito.listarProductos(this.productos);
+    //     console.log("Usted cargo: " + articulos + " productos y el total va a pagar es de : $" + total);
+    // }
+
+    agregarAlCarrito(carrito, producto) {
+        //verificarLocalStorage(carrito);
+        carrito.productos.push(producto);
+        localStorage.setItem('Carrito', JSON.stringify(this.productos));
     }
 
     TotalCarrito(productos) {
         let total = 0;
-
         for (const producto of productos) {
             total = total + producto.precio;
         }
@@ -68,31 +73,22 @@ class Tienda {
         this.telefono = telefono;
         this.baseDeDatos = baseDeDatos;
     }
-    filtrarProductoPorCategoria(valorgit) {
-        const encontrado = this.baseDeDatos.filter(producto => producto.categoria == valor);
+    filtrarProductoPorCategoria(valor) {
         let espacioProductos = document.getElementsByClassName("contenedorProductos")
-        for (const producto of encontrado) {
-            let contenedor = document.createElement("div");
-            contenedor.className = "col bg-dark text-white m-3 text-sm-center p-1";
-            contenedor.innerHTML = `
-                                    <img class="img-fluid" src="../img/${producto.img}" alt="bebida">
-                                    <p> ${producto.nombreProducto}</p>
-                                    <h4> $${producto.precio} </h4>
-                                    <button id="${producto.idProducto}" type="button" class="btn btn-primary btnComprar">Comprar</button>                                
-                                `;
-            espacioProductos[0].appendChild(contenedor);
-        }
-        console.log("Hola");
+        espacioProductos[0].innerHTML = "";
+        const encontrado = this.baseDeDatos.filter(producto => producto.categoria == valor);
+        this.listarProductos(encontrado);
     };
 
     buscarProductoPorId(valorId) {
         const encontrado = this.baseDeDatos.find(producto => producto.idProducto == valorId);
-        console.log(encontrado);
         return encontrado;
     };
 
-    listarProductos(espacioProductos) {
-        for (const producto of this.baseDeDatos) {
+    listarProductos(baseDeDatos) {
+        let espacioProductos = document.getElementsByClassName("contenedorProductos")
+        espacioProductos[0].innerHTML = "";
+        for (const producto of baseDeDatos) {
             let contenedor = document.createElement("div");
             contenedor.className = "col bg-dark text-white m-3 text-sm-center p-1";
             contenedor.innerHTML = `
@@ -107,37 +103,48 @@ class Tienda {
 }
 
 const baseDeDatos = [
-    { idProducto: 1, nombreProducto:"Pack 12 Cervezas Budweiser 710ml + Accesorios",marcaProducto: 'Budweiser', categoria: 'Cerveza', descripcionProducto: 'Lorem Ipsum is simply dummy text of the ', precio: 750, img:"pack12CervezasBudweiser710ml_Accesorios.png" },
-    { idProducto: 2, nombreProducto:"Pack 24 Cervezas Corona 710ml",marcaProducto: 'Corona', categoria: 'Cerveza', descripcionProducto: 'Lorem Ipsum is simply dummy text of the ', precio: 750, img:"cervezaCorona.png" },    
+    { idProducto: 1, nombreProducto: "Pack 12 Cervezas Budweiser 710ml + Accesorios", marcaProducto: 'Budweiser', categoria: 'Cerveza', descripcionProducto: 'Lorem Ipsum is simply dummy text of the ', precio: 750, img: "pack12CervezasBudweiser710ml_Accesorios.png" },
+    { idProducto: 2, nombreProducto: "Pack 24 Cervezas Corona 710ml", marcaProducto: 'Corona', categoria: 'Cerveza', descripcionProducto: 'Lorem Ipsum is simply dummy text of the ', precio: 750, img: "cervezaCorona.png" },
+    { idProducto: 2, nombreProducto: "Pack 24 Cervezas Corona 710ml", marcaProducto: 'Corona', categoria: 'Vino', descripcionProducto: 'Lorem Ipsum is simply dummy text of the ', precio: 750, img: "cervezaCorona.png" },
 ];
 
 const tienda = new Tienda('Brooklyn', 'Avellaneda 334', 4885548, baseDeDatos);
 
 function programa() {
     const carrito = new Carrito([]);
-    let espacioProductos = document.getElementsByClassName("contenedorProductos")
-    tienda.listarProductos(espacioProductos);
-    btnComprarOnClic();
+    verificarLocalStorage(carrito);
+    tienda.listarProductos(tienda.baseDeDatos);
+    btnComprarOnClic(carrito);
     selectFiltroOnChange();
 }
 
-function btnComprarOnClic() {
+function btnComprarOnClic(carrito) {
     let botones = document.getElementsByClassName('btnComprar');
     for (const boton of botones) {
-        boton.addEventListener('click', function(){
-            tienda.buscarProductoPorId(this.id)
-        } )
-        boton.addEventListener('click', function(){
-            //agregar al LocalStorage
-            
+        boton.addEventListener('click', function () {
+            let producto = tienda.buscarProductoPorId(this.id);
+            carrito.agregarAlCarrito(carrito, producto);
         })
     }
-    
 }
 
 function selectFiltroOnChange() {
     let filtroProductos = document.getElementById('filtroCategorias')
-    filtroProductos.addEventListener('change', function(){
-        tienda.filtrarProductoPorCategoria("Cerveza");
+    filtroProductos.addEventListener('change', function () {
+        if (this.value != "Todos") {
+            console.log(this.value)
+            tienda.filtrarProductoPorCategoria(this.value);
+        } else {
+            tienda.listarProductos(tienda.baseDeDatos);
+        }
     })
+}
+
+function verificarLocalStorage(carrito) {
+    if ('Carrito' in localStorage) {
+        const arrayT = JSON.parse(localStorage.getItem("Carrito"));
+        for (const producto of arrayT) {
+            carrito.productos.push(producto);
+        }
+    }
 }
